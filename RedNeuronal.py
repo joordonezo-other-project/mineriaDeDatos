3ca729da366a8885041202597a57377eb0578570
AlejandroAP98 <aalvarezpa@unal.edu.co>
2023-10-05 21:09:14 -0500

    cambio estructural de la neurona. capas ocultas y épocas

--- RedNeuronal.py
+++ RedNeuronal.py
@@ -3,115 +3,118 @@ import random
 import os
 import numpy as np
 
-
-# Fix random seed for reproducibility
+# Fijar una semilla aleatoria para reproducibilidad
 random.seed(113)
 
-#ruta al archivo CSV
-relative_path = 'data/IRIS.csv'
-absolute_path = os.path.dirname(__file__)
-full_path = os.path.join(absolute_path, relative_path)
+# Ruta al archivo CSV
+ruta_relativa = 'data/IRIS.csv'
+ruta_absoluta = os.path.dirname(__file__)
+ruta_completa = os.path.join(ruta_absoluta, ruta_relativa)
 
-# Load dataset
-with open(full_path) as csvfile:
+# Cargar el conjunto de datos
+with open(ruta_completa) as csvfile:
     csvreader = csv.reader(csvfile)
-    next(csvreader, None)  # skip header
+    next(csvreader, None)  # saltar la cabecera
     dataset = list(csvreader)
 
-# Change string value to numeric
+# Cambiar el valor de cadena a numérico
 class_mapping = {"Iris-setosa": 0, "Iris-versicolor": 1, "Iris-virginica": 2}
 for row in dataset:
     row[4] = class_mapping[row[4]]
     row[:4] = [float(row[j]) for j in range(len(row))]
 
-# Split x and y (feature and target)
+# Dividir x e y (características y objetivos)
 random.shuffle(dataset)
-split_ratio = 0.8
-split_index = int(len(dataset) * split_ratio)
-datatrain = dataset[:split_index]
-datatest = dataset[split_index:]
-train_X = [data[:4] for data in datatrain]
-train_y = [data[4] for data in datatrain]
-test_X = [data[:4] for data in datatest]
-test_y = [data[4] for data in datatest]
+ratio_division = 0.8
+indice_division = int(len(dataset) * ratio_division)
+datos_entrenamiento = dataset[:indice_division]
+datos_prueba = dataset[indice_division:]
+entrenamiento_X = [data[:4] for data in datos_entrenamiento]
+entrenamiento_y = [data[4] for data in datos_entrenamiento]
+prueba_X = [data[:4] for data in datos_prueba]
+prueba_y = [data[4] for data in datos_prueba]
 
-# Define MLP parameters
-input_size = 4
-hidden_size = 4
-output_size = 3
-learning_rate = 0.005
-epochs = 400
+# Definir los parámetros de la red neuronal MLP
+tamaño_entrada = 4
+tamaño_oculto = 8
+tamaño_salida = 3
+tasa_aprendizaje = 0.005
+épocas = 1000
 
-# Initialize weights and biases
-weights_input_hidden = np.random.uniform(-1, 1, (input_size, hidden_size))
-bias_hidden = np.zeros(hidden_size)
-weights_hidden_output = np.random.uniform(-1, 1, (hidden_size, output_size))
-bias_output = np.zeros(output_size)
+# Inicializar pesos y sesgos
+pesos_entrada_oculta = np.random.uniform(-1, 1, (tamaño_entrada, tamaño_oculto))
+sesgo_oculto = np.zeros(tamaño_oculto)
+pesos_oculta_salida = np.random.uniform(-1, 1, (tamaño_oculto, tamaño_salida))
+sesgo_salida = np.zeros(tamaño_salida)
 
-# Sigmoid activation function and its derivative
+# Función de activación sigmoide y su derivada
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-def sigmoid_derivative(x):
+# Derivada de la función sigmoide
+def derivada_sigmoid(x):
     return x * (1 - x)
 
-# Training loop
-for epoch in range(epochs):
-    total_error = 0
-    for i in range(len(train_X)):
-        # Forward propagation
-        hidden_input = np.dot(train_X[i], weights_input_hidden) + bias_hidden
-        hidden_output = sigmoid(hidden_input)
-        output_input = np.dot(hidden_output, weights_hidden_output) + bias_output
-        output = sigmoid(output_input)
+# Bucle de entrenamiento
+for época in range(épocas):
+    error_total = 0
+    for i in range(len(entrenamiento_X)):
+        # Propagación hacia adelante
+        entrada_oculta = np.dot(entrenamiento_X[i], pesos_entrada_oculta) + sesgo_oculto
+        salida_oculta = sigmoid(entrada_oculta)
+        entrada_salida = np.dot(salida_oculta, pesos_oculta_salida) + sesgo_salida
+        salida = sigmoid(entrada_salida)
 
-        # Compute the error
-        target = np.zeros(output_size)
-        target[int(train_y[i])] = 1  # Convert train_y[i] to an integer
-        error = target - output
-        total_error += np.sum(error**2)
+        # Calcular el error
+        objetivo = np.zeros(tamaño_salida)
+        objetivo[int(entrenamiento_y[i])] = 1
+        error = objetivo - salida
+        error_total += np.sum(error**2)
 
-        # Backpropagation
-        delta_output = error * sigmoid_derivative(output)
-        error_hidden = delta_output.dot(weights_hidden_output.T)
-        delta_hidden = error_hidden * sigmoid_derivative(hidden_output)
+        # Retropropagación
+        delta_salida = error * derivada_sigmoid(salida)
+        error_oculto = delta_salida.dot(pesos_oculta_salida.T)
+        delta_oculto = error_oculto * derivada_sigmoid(salida_oculta)
 
-        # Update weights and biases
-        weights_hidden_output += learning_rate * np.outer(hidden_output, delta_output)
-        bias_output += learning_rate * delta_output
-        weights_input_hidden += learning_rate * np.outer(train_X[i], delta_hidden)
-        bias_hidden += learning_rate * delta_hidden
+        # Actualizar pesos y sesgos
+        pesos_oculta_salida += tasa_aprendizaje * np.outer(salida_oculta, delta_salida)
+        sesgo_salida += tasa_aprendizaje * delta_salida
+        pesos_entrada_oculta += tasa_aprendizaje * np.outer(entrenamiento_X[i], delta_oculto)
+        sesgo_oculto += tasa_aprendizaje * delta_oculto
 
-    avg_error = total_error / len(train_X)
-    if epoch % 100 == 0:
-        print(f'Epoch {epoch}: Average Error = {avg_error:.6f}')
+    # Calcular el error promedio de la época
+    error_promedio = error_total / len(entrenamiento_X)
+    if época % 100 == 0:
+        print(f'Época {época}: Error Promedio = {error_promedio:.6f}')
+
+# Pruebas
+def predecir(X):
+    entrada_oculta = np.dot(X, pesos_entrada_oculta) + sesgo_oculto
+    salida_oculta = sigmoid(entrada_oculta)
+    entrada_salida = np.dot(salida_oculta, pesos_oculta_salida) + sesgo_salida
+    salida = sigmoid(entrada_salida)
+    return np.argmax(salida)
+
+predicciones = [predecir(x) for x in prueba_X]
+
+# Calcular la precisión
+precisión = sum(p == int(y) for p, y in zip(predicciones, prueba_y)) / len(prueba_y) * 100
+print(f'Precisión: {precisión:.2f}%')
 
 
-# Testing
-def predict(X):
-    hidden_input = np.dot(X, weights_input_hidden) + bias_hidden
-    hidden_output = sigmoid(hidden_input)
-    output_input = np.dot(hidden_output, weights_hidden_output) + bias_output
-    output = sigmoid(output_input)
-    return np.argmax(output)
-
-predictions = [predict(x) for x in test_X]
-
-# Calculate accuracy
-accuracy = sum(p == int(y) for p, y in zip(predictions, test_y)) / len(test_y) * 100
-print(f'Accuracy: {accuracy:.2f}%')
-
+# Predicción de un nuevo dato
 def pedir_datos():
     nuevo_dato = []
-    nuevo_dato.append(float(input("Sepal length: ")))
-    nuevo_dato.append(float(input("Sepal width: ")))
-    nuevo_dato.append(float(input("Petal length: ")))
-    nuevo_dato.append(float(input("Petal width: ")))
+    nuevo_dato.append(float(input("Longitud del Sépalo: ")))
+    nuevo_dato.append(float(input("Ancho del Sépalo: ")))
+    nuevo_dato.append(float(input("Longitud del Pétalo: ")))
+    nuevo_dato.append(float(input("Ancho del Pétalo: ")))
     return nuevo_dato
 
-nuevo_dato  = pedir_datos()
-class_mapping = {0: "Iris-Setosa", 1 : "Iris-Versicolor", 2:"Iris-Virginica"}
-etiqueta_predicha = predict(nuevo_dato)
-print(class_mapping[etiqueta_predicha])
+nuevo_dato = pedir_datos()
+mapeo_clases = {0: "Iris-Setosa", 1: "Iris-Versicolor", 2: "Iris-Virginica"}
+etiqueta_predicha = predecir(nuevo_dato)
+print(mapeo_clases[etiqueta_predicha])
+
 
     
